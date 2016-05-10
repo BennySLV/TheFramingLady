03b05715da264ae90f9419c746724868e2db34e7
Benjamin Silveston <bsilveston37@gmail.com>
2016-05-10 15:14:31 +0100

    Updated comments on portfolio .js script.

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,67 +1,69 @@
 /* JavaScript/jQuery for the portfolio page */
+
+// Shorthand for $(document).ready()
 $(function() {
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-1').on('click', function() {
 		$('#img-1-text-description').addClass('description');
 		$('#img-1-text-description').slideDown('slow').load('custom-text/img-1-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-1').on('dblclick', function() {
 		$('#img-1-text-description').slideUp('slow');		
 	});
 
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-2').on('click', function() {
 		$('#img-2-text-description').addClass('description');
 		$('#img-2-text-description').slideDown('slow').load('custom-text/img-2-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-2').on('dblclick', function() {
 		$('#img-2-text-description').slideUp('slow');
 	});
 
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-3').on('click', function() {
 		$('#img-3-text-description').addClass('description');
 		$('#img-3-text-description').slideDown('slow').load('custom-text/img-3-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-3').on('dblclick', function() {
 		$('#img-3-text-description').slideUp('slow');
 	});
 
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-4').on('click', function() {
 		$('#img-4-text-description').addClass('description');
 		$('#img-4-text-description').slideDown('slow').load('custom-text/img-4-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-4').on('dblclick', function() {
 		$('#img-4-text-description').slideUp('slow');
 	});
 
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-5').on('click', function() {
 		$('#img-5-text-description').addClass('description');
 		$('#img-5-text-description').slideDown('slow').load('custom-text/img-5-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-5').on('dblclick', function() {
 		$('#img-5-text-description').slideUp('slow');
 	});
 
-	// Show caption when user hovers over image
+	// Show caption when user clicks on image
 	$('#img-6').on('click', function() {
 		$('#img-6-text-description').addClass('description');
 		$('#img-6-text-description').slideDown('slow').load('custom-text/img-6-description.html');
 	});
 
-	// Remove caption when the user leaves the image
+	// Remove caption when the double-clicks on the image
 	$('#img-6').on('dblclick', function() {
 		$('#img-6-text-description').slideUp('slow');
 	});
